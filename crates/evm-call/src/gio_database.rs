4c4be10c5bf34aa1bee0fa7c98a806126c434846
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-04-14 21:00:59 +0800

    implement basic_async_ref

--- crates/evm-call/src/gio_database.rs
+++ crates/evm-call/src/gio_database.rs
@@ -4,7 +4,7 @@ use revm::{
     state::{AccountInfo, Bytecode},
 };
 
-use alloy_primitives::BlockHash;
+use alloy_primitives::{BlockHash, Bytes};
 
 use crate::{
     gio_client::{GIOClient, GIODomain, GIOHash, GIOHint},
@@ -55,7 +55,39 @@ fn concat_bytes(v1: &Vec<u8>, v2: &Vec<u8>) -> Vec<u8> {
 impl DatabaseAsyncRef for GIODatabase {
     type Error = GIOError;
 
-    async fn basic_async_ref(&self, address: Address) -> Result<Option<AccountInfo>, Self::Error> {}
+    async fn basic_async_ref(&self, address: Address) -> Result<Option<AccountInfo>, Self::Error> {
+        let data = concat_bytes(&self.block_hash.to_vec(), &address.to_vec());
+        let response = self.client.emit_gio(GIODomain::PreimageHint, &data).await?;
+        if !response.is_ok() {
+            return Err(GIOError::BadResponse {
+                message: "failed to get account".to_string(),
+                response_code: response.code,
+            });
+        }
+
+        let balance_data: [u8; 32] = response.data[0..32]
+            .try_into()
+            .expect("invalid account balance data length");
+        let nonce_data: [u8; 8] = response.data[32..40]
+            .try_into()
+            .expect("invalid account nonce data length");
+        let code_hash_data: [u8; 32] = response.data[40..72]
+            .try_into()
+            .expect("invalid account account code hash data length");
+
+        self.emit_hint(GIOHint::EthCodePreimage, &address.to_vec())
+            .await?;
+        let code_data = self.get_preimage(B256::from(code_hash_data)).await?;
+
+        let account = AccountInfo {
+            balance: U256::from_le_bytes(balance_data),
+            nonce: u64::from_le_bytes(nonce_data),
+            code_hash: B256::from(code_hash_data),
+            code: Some(Bytecode::new_raw(Bytes::from(code_data))),
+        };
+
+        Ok(Some(account))
+    }
 
     async fn code_by_hash_async_ref(&self, _code_hash: B256) -> Result<Bytecode, Self::Error> {}
 
